f4ea74ab33e35f8696d30de85e7263ad3e84f418
Andrey Egorov <a-n-egorov@yandex.ru>
2023-12-10 13:38:26 +0300

    Added pages for errors

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundUserPage = () => {
+  return (
+    <div>The user doesn&apos;t exist</div>
+  )
+}
+
+export default NotFoundUserPage
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserTable from './UserTable'
+import Link from 'next/link';
 
 interface Props {
   searchParams: {sortOrder: string};
@@ -9,6 +10,7 @@ const UsersPage = async ({searchParams: {sortOrder}}: Props) => {
   return (
     <>
     <div>UsersPage sorted by {sortOrder}</div>
+    <Link href="/users/new" className='btn btn-primary'>New User</Link>
     <UserTable sortOrder={sortOrder}/>
     </>
   )
